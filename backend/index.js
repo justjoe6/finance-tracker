232255b50cc72f7ca85b4519e8c6dc4ed09b71df
Joseph Ranieri <joeran65@gmail.com>
2025-02-08 14:52:46 -0500

    Implemented login page design and set up back end to handle api calls for sign up

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,19 @@
-const express = include("express");
+const express = require("express");
+require("./db/config.js");
+const User = require("./db/user");
 const app = express();
+const cors = require("cors")
+
+app.use(express.json())
+app.use(cors())
+
+app.post("/signup", async (req,rsp)=>{
+    let user = new User(req.body)
+    let result = await user.save()
+    result = result.toObject()
+    delete result.password
+    rsp.send(result)
+})
+
+
+app.listen(5000);
