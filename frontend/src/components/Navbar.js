62fa81269bc967b417c485a24639a53a5b55b2c1
justjoe6 <68125991+justjoe6@users.noreply.github.com>
2025-02-27 16:27:53 -0500

    Updated Navbar with updated routes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,17 +7,17 @@ const NavBar = () =>{
     const navigate = useNavigate()
     const logout = ()=>{
         localStorage.clear()
-        navigate("/")
+        navigate("/login")
     }
 
     return (<div className="nav"><h2>Finance Tracker</h2>
     <div className="nav-right">
     {loggedIn ?
     <>
-    <Link className="navLinks" to="/home"><p>home</p></Link>
+    <Link className="navLinks" to="/"><p>home</p></Link>
     <Link className="navLinks" to="/profile"><p>profile</p></Link>
     <Link onClick={logout} className="navLinks" to="/"><p>logout</p></Link></>:
-    <><Link className="navLinks" to="/"><p>login</p></Link>
+    <><Link className="navLinks" to="/login"><p>login</p></Link>
     <Link className="navLinks" to="/signup"><p>signup</p></Link></>}
     </div>
     </div>)
